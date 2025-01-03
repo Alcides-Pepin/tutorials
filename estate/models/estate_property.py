f0c8086e407934d5d9872fc950cad68f4495c5ec
Alcides <galcides.pepin@gmail.com>
2025-01-03 08:43:54 +0100

    [edit] added whole chapter 9

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,10 +1,11 @@
-from odoo import fields, models
+from odoo import fields, models, api
 from odoo.tools import date_utils
+from odoo.exceptions import UserError
 
 
 class EstateProperty(models.Model):
-    _name = "estate.property"
-    _description = "Estate Property model"
+    _name = 'estate.property'
+    _description = 'Estate Property model'
 
     name = fields.Char(required=True)
     description = fields.Text()
@@ -45,15 +46,60 @@ class EstateProperty(models.Model):
         copy=False,
         default='new'
         )
-    property_type_id = fields.Many2one(string="Property Type",
-                                       comodel_name="estate.property.type")
-    sales_person_id = fields.Many2one(string="Sales Person",
-                                      comodel_name="res.users",
+    property_type_id = fields.Many2one(string='Property Type',
+                                       comodel_name='estate.property.type')
+    sales_person_id = fields.Many2one(string='Sales Person',
+                                      comodel_name='res.users',
                                       default=lambda self: self.env.user)
-    buyer_id = fields.Many2one(string="Buyer",
-                               comodel_name="res.partner",
+    buyer_id = fields.Many2one(string='Buyer',
+                               comodel_name='res.partner',
                                copy=False)
-    tag_ids = fields.Many2many(string="Tags",
-                               comodel_name="estate.property.tag")
-    offer_ids = fields.One2many("estate.property.offer", "property_id",
-                                string="Offers")
+    tag_ids = fields.Many2many(string='Tags',
+                               comodel_name='estate.property.tag')
+    offer_ids = fields.One2many('estate.property.offer', 'property_id',
+                                string='Offers')
+
+    total_area = fields.Float(string='Total Area',
+                              compute='_compute_total_area')
+
+    best_price = fields.Float(string='Best Price',
+                              compute='_compute_best_price')
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        # self.total_area = self.garden_area + self.living_area
+
+        for record in self:
+            record.total_area = record.garden_area + record.living_area
+
+    # # mapped() apply a function to every record of a recordset
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_price = max(record.mapped('offer_ids.price'))
+            else:
+                record.best_price = 0
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = False
+            self.garden_orientation = False
+
+    def action_sell_property(self):
+        for record in self:
+            if record.state == 'cancelled':
+                raise UserError(("You cannot define a cancelled property as sold."))
+            else:
+                record.state = 'sold'
+
+    def action_cancel_property(self):
+        for record in self:
+            if record.state == 'sold':
+                raise UserError(("You cannot define a sold property as cancelled."))
+            else:
+                record.state = 'cancelled'
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,9 +1,11 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from datetime import timedelta
+from odoo.exceptions import UserError
 
 
 class EstatePropertyOffer(models.Model):
-    _name = "estate.property.offer"
-    _description = "Estate Property Offer model"
+    _name = 'estate.property.offer'
+    _description = 'Estate Property Offer model'
 
     price = fields.Float(
         string='Price',
@@ -18,11 +20,51 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one(
         string='Buyer',
-        comodel_name="res.partner",
+        comodel_name='res.partner',
         required=True
     )
     property_id = fields.Many2one(
         string='Property',
-        comodel_name="estate.property",
+        comodel_name='estate.property',
         required=True
     )
+    validity = fields.Integer(
+        string='Validity',
+        default=7)
+
+    date_deadline = fields.Date(
+        string='Date Deadline',
+        compute='_compute_date_deadline',
+        inverse='_inverse_date_deadline',
+        )
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        # print("Méthode compute appelée")
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + timedelta(days=record.validity)
+            else:
+                record.date_deadline = fields.Date().today() + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        # print("Méthode inverse appelée")
+        for record in self:
+            if record.create_date:
+                create_date = record.create_date.date()
+                record.validity = (record.date_deadline - create_date).days
+            else:
+                record.validity = (record.date_deadline - fields.Date().today()).days
+
+    def action_accept_offer(self):
+        if self.property_id.offer_ids.filtered(lambda offer: offer.status == 'accepted'):
+            raise UserError(("Another offer has already been accepted."))
+        else:
+            self.status = 'accepted'
+            self.property_id.selling_price = self.price
+            self.property_id.buyer_id = self.partner_id
+
+    def action_refuse_offer(self):
+        self.status = 'refused'
+        self.property_id.selling_price = False
+        self.property_id.buyer_id = False
