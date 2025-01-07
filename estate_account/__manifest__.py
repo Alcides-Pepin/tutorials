aed03d40ff64b8373501041f5b8b24b7d2076a4f
Alcides <galcides.pepin@gmail.com>
2025-01-07 08:20:06 +0100

    [edit] estate account creation

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Real Estate Account',
+    'version': '18.0.1.0',
+    'category': 'Customizations',
+    'sequence': 10,
+    'summary': 'Create an invoice for sold properties in the appropriate module.',
+    'description': """
+    Create an invoice for sold properties in the appropriate module.
+    """,
+    'author': 'Pépin, Alcides DE OLIVEIRA GUERRA',
+    'depends': [
+        'base',
+        'account',
+        'estate',
+        ],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
